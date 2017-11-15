6d7c786d8cca4930e8da7071ecaa1047bacaad2d
TheNodeCoin <xander@bloodwillow.com>
2017-11-15 10:49:41 -0700

    rebuild

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -24,7 +24,7 @@ const size_t   BLOCKCHAIN_TIMESTAMP_CHECK_WINDOW             = 60;
 //TODO Specify total number of available coins
 //TODO ((uint64_t)(-1)) equals to 18446744073709551616 coins
 //TODO or you can define number explicitly UINT64_C(858986905600000000)
-const uint64_t MONEY_SUPPLY                                  = 858986905600000000;
+const uint64_t MONEY_SUPPLY                                  = 3300000000000;
 const unsigned EMISSION_SPEED_FACTOR                         = 21;
 static_assert(EMISSION_SPEED_FACTOR <= 8 * sizeof(uint64_t), "Bad EMISSION_SPEED_FACTOR");
 
@@ -74,7 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "eBucks";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff0001f8a181eff50b029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101fe85dc73faa405f3c83ee0a68f8b7660b54a2328006474aed4dbe35418b69162";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff0001ba8560029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd088071210141fdb988298464cd52629f3e7cba4f3b101448976d6df7e378c3c49528906c51";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
